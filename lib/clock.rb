c0a9f272a179ce1af3eb7fc46fccfacd9ddd0799
Michael J Dowling <trubld.mynd@gmail.com>
2014-08-28 21:03:40 +1000

    Clock gem set for 2 mins

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,17 +1,7 @@
 require 'clockwork'
-module Clockwork
-  handler do |job|
-    puts "Running #{job}"
-  end
+require './config/boot'
+require './config/environment'
 
-  # handler receives the time when job is prepared to run in the 2nd argument
-  # handler do |job, time|
-  #   puts "Running #{job}, at #{time}"
-  # end
+include Clockwork
 
-  every(10.seconds, 'frequent.job')
-  every(3.minutes, 'less.frequent.job')
-  every(1.hour, 'hourly.job')
-
-  every(1.day, 'midnight.job', :at => '00:00')
-end
+every(2.minutes, 'Queueing interval job') { Delayed::Job.enqueue IntervalJob.new }
